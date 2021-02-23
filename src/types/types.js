422cb990792eeeedd13198fb3801fec49c097433
robertramosastudillo <robertramosastudillo@gmail.com>
2021-02-23 14:29:32 -0500

    Se trabajo en el proceso de autenticacion entre frontend y backend

--- src/types/types.js
+++ src/types/types.js
@@ -7,4 +7,12 @@ export const types = {
   eventClearActiveEvent: "[event] Clear active event",
   eventUpdated: "[event] Event updated",
   eventDeleted: "[event] Event deleted",
+
+  authChecking: "[auth] Checking login state",
+  authCheckingFinish: "[auth] Finish checking login state",
+  authStartLogin: "[auth] Start login",
+  authLogin: "[auth] Login",
+  authStartRegister: "[auth] Start Register",
+  authStartTokenRenew: "[auth] Start Renew",
+  authLogout: "[auth] Logout",
 };
